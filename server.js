c42567183b8f529d798927362236aae621c8f8e0
Issouf Zabre <sheridanzabre@gmail.com>
2020-02-03 20:33:47 -0500

    Added git ignore and removed node_modules

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const speech = require('@google-cloud/speech').v1p1beta1;
 const fs = require('fs');
+var cors = require('cors')
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json({limit: '150mb'}));
 app.use(express.urlencoded({limit: '150mb'}));
 
