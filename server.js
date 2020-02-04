67b663d59b76a39394cbfe08125588e090bf16ad
Issouf Zabre <sheridanzabre@gmail.com>
2020-02-03 20:55:26 -0500

    heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 var cors = require('cors')
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json({limit: '150mb'}));
